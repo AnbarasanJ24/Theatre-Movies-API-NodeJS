43750fcef626d3e102bca27261c13dfa29a4306c
Anbarasan <anbarasanj24@gmail.com>
2021-10-08 22:17:14 +0530

    Added select, filter, sorting and pagination features to the theatre API

--- controllers/theatres.js
+++ controllers/theatres.js
@@ -9,17 +9,72 @@ const ErrorResponse = require("../utilis/ErrorResponse");
 // @access    Public
 exports.getTheatres = asyncHandler(async (req, res, next) => {
     console.log(req.query)
-    let query;
 
-    let queryStr = JSON.stringify(req.query);
+    // Taking copy to avoid passing unnecessary field to find query
+    let reqQuery = { ...req.query };
+
+    // Removing fields in copied req.query
+    let removeFields = ['select', 'sort', 'page', 'limit', 'skip'];
+    removeFields.forEach(params => delete reqQuery[params]);
+
+    // Making comparison operator query
+    let query;
+    let queryStr = JSON.stringify(reqQuery);
     queryStr = queryStr.replace(/\b(eq|neq|gt|gte|lt|lte)\b/g, match => `$${match}`)
     query = Theatre.find(JSON.parse(queryStr));
 
+    // select query (Need to send required fields for display)
+    if (req.query.select) {
+        const selectQuery = req.query.select.split(',').join(' ');
+        query = query.select(selectQuery);
+    }
+
+    // sorting (value -> 1(Ascending) & -1 descending)
+    // ascending sort=name and descending = -name
+    if (req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+    } else {
+        query = query.sort('-createdAt')
+    }
+
+    // Pagination & Limit
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 1;
+    // No of Documents to be skipped
+    // For second page=> (2-1)*10 = 10 (So 10 documents will be skipped)
+    // startIndex =10 and endIndex=20
+    // Assume we have total 40 records
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await Theatre.countDocuments();
+
+    query = query.skip(startIndex).limit(limit);
+
     const data = await query;
+
+    // Creating pagination object
+    const pagination = {};
+
+    if (endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit
+        }
+    }
+
+    if (startIndex > 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit
+        }
+    }
 
     res.status(200).json({
         success: true,
-        data: data
+        data: data,
+        count: total,
+        pagination
     });
 })
 
@@ -87,3 +142,10 @@ exports.deleteTheatre = asyncHandler(async (req, res, next) => {
         next(err);
     }
 })
+
+
+/*
+find : /api/v1/theatres?averageTicketCost[gte]=200&phone=94565857496
+select: /api/v1/theatres?select=name,description
+select & sort : /api/v1/theatres?select=name,description&sort=-name
+*/
